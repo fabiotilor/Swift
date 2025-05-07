bba1e801e7139c3943c648c510ef427b58ad8a9c
fabiotilor <fabio.dimeo13@gmail.com>
2025-05-07 17:05:44 +0200

    Initial Commit

---
+++ QPLearn/QPLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  QPLearnApp.swift
+//  QPLearn
+//
+//  Created by Fabio Di Meo on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct QPLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
