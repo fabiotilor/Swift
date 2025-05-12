36f1ef8d32c352596a2dd57952d85eab420c8468
fabiotilor <fabio.dimeo13@gmail.com>
2025-05-12 09:25:02 +0200

    Adding files

---
+++ Timer/Timer/Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Fabio Di Meo on 07.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Timer/Timer/TimerTests/TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTests.swift
+//  TimerTests
+//
+//  Created by Fabio Di Meo on 07.05.2025.
+//
+
+import Testing
+@testable import Timer
+
+struct TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
